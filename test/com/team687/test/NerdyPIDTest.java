cb242e99bdc9350c705b03cb62f4c50eb4dad2a3
tedfoodlin <tedklin@gmail.com>
2017-07-05 09:42:51 -0700

    yay unit tests

---
+++ test/com/team687/test/NerdyPIDTest.java
@@ -0,0 +1,24 @@
+package com.team687.test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.team687.frc2017.utilities.*;
+
+public class NerdyPIDTest {
+	
+    private static final double kEpsilon = 1E-9;
+	
+	@Test
+	public void testNerdyPID() {
+		NerdyPID nerdyTestPID = new NerdyPID(0.0687, 0, 0);
+		nerdyTestPID.setOutputRange(0.254, 0.971);
+		nerdyTestPID.setDesired(0);
+		
+		assertEquals(0.971, nerdyTestPID.calculate(-687), kEpsilon);
+		assertEquals(-0.971, nerdyTestPID.calculate(687), kEpsilon);
+		assertEquals(0.254, nerdyTestPID.calculate(-0.001), kEpsilon);
+		assertEquals(-0.254, nerdyTestPID.calculate(0.001), kEpsilon);
+	}
+
+}
