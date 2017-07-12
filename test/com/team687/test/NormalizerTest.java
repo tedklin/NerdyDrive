b2949964bfaf21dc7970b9cedbc4a23decaec6e3
tedfoodlin <tedklin@gmail.com>
2017-07-12 10:45:07 -0700

    add normalizer test (woo first parameterized test)

---
+++ test/com/team687/test/NormalizerTest.java
@@ -0,0 +1,57 @@
+package com.team687.test;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.team687.frc2017.utilities.*;
+
+/**
+ * Test for normalizer
+ * 
+ * @author tedfoodlin
+ *
+ */
+
+@RunWith(Parameterized.class)
+public class NormalizerTest {
+    
+    @SuppressWarnings("rawtypes")
+	@Parameters
+    public static Collection testCases() {
+    		return Arrays.asList(new double[][] {
+    				{0.971, 0.987}, {0.971, 1.678}, {1.678, 0.971}, {-0.971, 1.678}, 
+    				{0.971, -1.678}, {1.678, 2.54}, {2.54, 1.678}, {687, 254}
+    		});
+    }
+    
+    private double[] m_rawVal;
+    
+    public NormalizerTest(double[] rawVal) {
+    		m_rawVal = rawVal;
+    }
+    
+    private static final double kEpsilon = 1E-9;
+    
+    @Test
+    public void normalizeTest() {
+		double[] normalizedVal = NerdyMath.normalize(m_rawVal, false);
+		for (int i = 0; i < normalizedVal.length - 1; i++) {
+			assertTrue(Math.abs(normalizedVal[i]) <= 1.0);
+		}
+		assertEquals((m_rawVal[0]/m_rawVal[1]), (normalizedVal[0]/normalizedVal[1]), kEpsilon);
+		assertTrue(isNegative(m_rawVal[0]) == isNegative(normalizedVal[0]));
+		assertTrue(isNegative(m_rawVal[1]) == isNegative(normalizedVal[1]));
+    }
+    
+    public static boolean isNegative(double d) {
+        return Double.doubleToRawLongBits(d) < 0;
+   }
+
+}
