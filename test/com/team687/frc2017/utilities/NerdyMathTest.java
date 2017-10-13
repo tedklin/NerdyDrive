c9667357e9136ffaf9bd63b07d5a103da4b93dae
tedfoodlin <tedklin@gmail.com>
2017-10-13 13:26:50 -0700

    threshold test

--- test/com/team687/frc2017/utilities/NerdyMathTest.java
+++ test/com/team687/frc2017/utilities/NerdyMathTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -21,33 +22,57 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class NerdyMathTest {
 
+    private static final double kEpsilon = 1E-9;
+
     enum Type {
 	NORMALIZE, THRESHOLD
     };
 
     @SuppressWarnings("rawtypes")
     @Parameters
-    public static Collection testCases() {
-	return Arrays.asList(new double[][] { { 0.971, 0.987 }, { 0.971, 1.678 }, { 1.678, 0.971 }, { -0.971, 1.678 },
-		{ 0.971, -1.678 }, { 1.678, 2.54 }, { 2.54, 1.678 }, { 687, 254 } });
+    public static Collection<Object[]> testCases() {
+	return Arrays.asList(new Object[][] { { Type.NORMALIZE, 0.971, 0.987, 1 }, { Type.NORMALIZE, 0.971, 1.678, 1 },
+		{ Type.NORMALIZE, 1.678, 0.971, 1 }, { Type.NORMALIZE, -0.971, 1.678, 1 },
+		{ Type.NORMALIZE, 0.971, -1.678, 1 }, { Type.NORMALIZE, 1.678, 2.54, 1 },
+		{ Type.NORMALIZE, 2.54, 1.678, 1 }, { Type.NORMALIZE, 687, 254, 1 },
+		{ Type.THRESHOLD, -0.971, 0.254, 0.987 }, { Type.THRESHOLD, -1.114, 0.254, 0.987 },
+		{ Type.THRESHOLD, -0.118, 0.254, 0.987 }, { Type.THRESHOLD, 0.971, 0.254, 0.987 },
+		{ Type.THRESHOLD, 0.118, 0.254, 0.987 }, { Type.THRESHOLD, 1.114, 0.254, 0.987 } });
     }
 
+    private Type m_type;
+    private double m_firstVal;
+    private double m_secondVal;
+    private double m_thirdVal;
     private double[] m_rawVal;
 
-    public NerdyMathTest(double[] rawVal) {
-	m_rawVal = rawVal;
+    public NerdyMathTest(Type type, double firstVal, double secondVal, double thirdVal) {
+	m_type = type;
+	m_firstVal = firstVal;
+	m_secondVal = secondVal;
+	m_thirdVal = thirdVal;
     }
-
-    private static final double kEpsilon = 1E-9;
 
     @Test
     public void normalizeTest() {
+	Assume.assumeTrue(m_type == Type.NORMALIZE);
+	m_rawVal = new double[] { m_firstVal, m_secondVal }; // represents left and right power
 	double[] normalizedVal = NerdyMath.normalize(m_rawVal, false);
 	for (int i = 0; i < normalizedVal.length - 1; i++) {
 	    assertTrue(Math.abs(normalizedVal[i]) <= 1.0);
 	    assertTrue(isNegative(m_rawVal[i]) == isNegative(normalizedVal[i]));
 	}
 	assertEquals((m_rawVal[0] / m_rawVal[1]), (normalizedVal[0] / normalizedVal[1]), kEpsilon);
+    }
+
+    @Test
+    public void thresholdTest() {
+	Assume.assumeTrue(m_type == Type.THRESHOLD);
+	double value = m_firstVal;
+	double minimum = m_secondVal;
+	double maximum = m_thirdVal;
+	double thresholdedVal = NerdyMath.threshold(value, minimum, maximum);
+	assertTrue(minimum <= Math.abs(thresholdedVal) && Math.abs(thresholdedVal) <= maximum);
     }
 
     public static boolean isNegative(double d) {
