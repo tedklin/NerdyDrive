871886b8644abdca0f6fa3871a8a5745e55d101e
tedfoodlin <tedklin@gmail.com>
2017-10-14 13:01:32 -0700

    fix bezier curve unit test, added example paths to test

--- test/com/team687/frc2017/utilities/BezierCurveTest.java
+++ test/com/team687/frc2017/utilities/BezierCurveTest.java
@@ -25,11 +25,17 @@ import com.team687.frc2017.Constants;
 @RunWith(Parameterized.class)
 public class BezierCurveTest {
 
+    private final static double[] EmptyPath = { 0, 0, 0, 0, 0, 0, 0, 0 };
+    private final static double[] ExamplePath1 = { 0, 0, -39000, 40000, -39000, 64000, -39000, 101000 };
+    private final static double[] ExamplePath2 = { 0, 0, 0, 100300, 0, 100300, -48000, 100900 };
+    private final static double[] ExamplePath3 = { 0, 0, 0, 92278.44, 0, 92278.44, 74403.76, 134999.94 };
+    private final static double[] ExamplePath4 = { 74403.7644, 134999.94, 0, 86582.24, 0, 198227.76, -33835.428,
+	    -117615.1376 };
+
     @SuppressWarnings("rawtypes")
     @Parameters
     public static Collection testCases() {
-	return Arrays.asList(new double[][] { Constants.BluePathWallToHopper1678, Constants.BluePathWallToHopper2056,
-		Constants.BluePathWallToHopper973, Constants.BluePathWallToClosePeg, Constants.BluePathPegToHopper });
+	return Arrays.asList(new double[][] { EmptyPath, ExamplePath1, ExamplePath2, ExamplePath3, ExamplePath4 });
     }
 
     private double[] m_path;
@@ -70,7 +76,7 @@ public class BezierCurveTest {
     }
 
     @Test
-    public void testDynamicStraightPower() {
+    public void testPathFollower() {
 	BezierCurve bezierCurve = new BezierCurve(m_path);
 	bezierCurve.calculateBezier();
 
@@ -87,15 +93,14 @@ public class BezierCurveTest {
 		straightPower = Constants.kBezierDistHighGearPGains.getMaxPower() * sign;
 	    }
 
-	    // concept: straight power is base, rot power is added as adjustment for heading
 	    double leftPower = straightPower + rotPower;
 	    double rightPower = straightPower - rotPower;
 
 	    System.out.println("Left Power: " + leftPower);
 	    System.out.println("Right Power: " + rightPower);
 
-	    assertTrue(-0.5 <= leftPower && leftPower <= 1.0);
-	    assertTrue(-0.5 <= rightPower && rightPower <= 1.0);
+	    assertTrue(0 <= leftPower && leftPower <= 1.0);
+	    assertTrue(0 <= rightPower && rightPower <= 1.0);
 	}
     }
 
