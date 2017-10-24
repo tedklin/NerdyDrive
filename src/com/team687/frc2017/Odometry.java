afee69dc0dd7f994940574b7e2a11a4ad79e7cc1
tedfoodlin <tedklin@gmail.com>
2017-10-23 20:30:27 -0700

    add pose estimation based on position

--- src/com/team687/frc2017/Odometry.java
+++ src/com/team687/frc2017/Odometry.java
@@ -18,23 +18,18 @@ public class Odometry {
 
     private static Odometry m_instance = null;
 
-    private double m_gyroYawDegrees;
-    private double m_gyroYawRadians;
+    private double m_gyroYawDegrees, m_gyroYawRadians;
     private double m_derivedYaw;
     private double m_angularVelocity;
     private double m_arcRadius;
 
-    private double m_leftDistance;
-    private double m_rightDistance;
-    private double m_leftVelocity;
-    private double m_rightVelocity;
+    private double m_leftDistance, m_rightDistance;
+    private double m_lastLeftDistance, m_lastRightDistance;
+    private double m_leftVelocity, m_rightVelocity;
 
-    private double m_currentTime;
-    private double m_lastTime;
-    private double m_deltaTime;
+    private double m_currentTime, m_lastTime, m_deltaTime;
 
-    private Pose m_lastPose;
-    private Pose m_newPose;
+    private Pose m_lastPose, m_newPose;
 
     public static Odometry getInstance() {
 	if (m_instance == null) {
@@ -47,12 +42,15 @@ public class Odometry {
 	m_lastTime = Timer.getFPGATimestamp();
 
 	// starting configuration
+	m_newPose = new Pose(0, 0, Robot.drive.getCurrentYawRadians());
 	m_lastPose = new Pose(0, 0, Robot.drive.getCurrentYawRadians());
 	m_derivedYaw = Robot.drive.getCurrentYawRadians();
     }
 
+    /**
+     * Update raw sensor readings and inherent calculations
+     */
     public void update() {
-	// raw sensor readings
 	m_leftDistance = Robot.drive.getLeftTicks();
 	m_rightDistance = Robot.drive.getRightTicks();
 	m_leftVelocity = Robot.drive.getLeftTicksSpeed();
@@ -72,7 +70,6 @@ public class Odometry {
 	SmartDashboard.putNumber("Accel Y", Robot.drive.getCurrentAccelY());
 	SmartDashboard.putNumber("Accel Z", Robot.drive.getCurrentAccelZ());
 
-	// calculations
 	m_derivedYaw += Kinematics.getDerivedDeltaYaw(m_rightVelocity, m_leftVelocity, m_deltaTime);
 	m_angularVelocity = Kinematics.getAngularVelocity(m_rightVelocity, m_leftVelocity);
 	m_arcRadius = Kinematics.getCurvatureRadius(m_rightVelocity, m_leftVelocity);
@@ -81,18 +78,44 @@ public class Odometry {
 	SmartDashboard.putNumber("Angular Velocity", m_angularVelocity);
 	SmartDashboard.putNumber("Radius of Curvature", m_arcRadius);
 
+	estimatePoseVelocity();
+	// estimatePosePosition();
+
+	SmartDashboard.putNumber("X", m_newPose.getX());
+	SmartDashboard.putNumber("Y", m_newPose.getY());
+	SmartDashboard.putNumber("Theta (radians)", m_newPose.getTheta()); // this is the same as yaw in radians
+
+    }
+
+    /**
+     * Pose estimation based on velocity
+     */
+    public void estimatePoseVelocity() {
 	m_currentTime = Timer.getFPGATimestamp();
 	m_deltaTime = m_currentTime - m_lastTime;
 	m_lastTime = m_currentTime;
 
 	m_newPose = Kinematics.getNewPose(m_lastPose, m_rightVelocity, m_leftVelocity, m_deltaTime);
 	m_lastPose = m_newPose;
-
-	SmartDashboard.putNumber("X", m_newPose.getX());
-	SmartDashboard.putNumber("Y", m_newPose.getY());
-	SmartDashboard.putNumber("Theta (radians)", m_newPose.getTheta()); // this is the same as yaw in radians
     }
 
+    /**
+     * Pose estimation based on position instead of velocity
+     * 
+     * May be more robust but less accurate.
+     */
+    public void estimatePosePosition() {
+	double deltaDistance = ((m_leftDistance - m_lastLeftDistance) + (m_rightDistance - m_lastRightDistance)) / 2;
+	m_newPose.setX(m_lastPose.getX() + deltaDistance * Math.sin(m_gyroYawRadians));
+	m_newPose.setY(m_lastPose.getY() + deltaDistance * Math.cos(m_gyroYawRadians));
+	m_newPose.setTheta(m_gyroYawRadians);
+
+	m_lastPose = m_newPose;
+    }
+
+    /**
+     * @return current estimated pose
+     */
     public Pose getCurrentPose() {
 	return m_newPose;
     }
